d385ba0e9b6751d14165a16f82e0f00ed9ff3229
Viitalii Znak <vitaliiznak@users.noreply.github.com>
2024-03-18 21:16:22 +0100

    Fix CI permissions

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   reactStrictMode: true,
   output: "export",
-  basePath:"/test-guess-the-country-game"
+  basePath: process.env.NODE_ENV === 'production' ?  "/test-guess-the-country-game" : ''
 };
